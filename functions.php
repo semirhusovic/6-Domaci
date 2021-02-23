63b16000b3119e1636a23dd36b746a151d1bb8a5
Semir Husovic <semir.husovic18@gmail.com>
2021-02-23 12:36:13 +0100

    Image deletion added

--- functions.php
+++ functions.php
@@ -10,5 +10,19 @@ function getByID($pdo, $table, $wfield, $uslov, $value)
     }
 }
 
-getByID($pdo, "grad", "grad", "id_grad", 1);
-getByID($pdo, "tip_nekretnine", "tip_nekretnine", "id_tip_nekretnine", 1);
+function deleteImages($pdo, $id_nekretnine)
+{
+    $stmt = $pdo->prepare("SELECT * FROM fotografije WHERE id_nekretnina = $id_nekretnine ");
+    if (!$stmt->execute()) {
+        echo $stmt->errorCode();
+    } else {
+        while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            unlink("pages/" . $result['slika']);
+            echo "Obrisana slika: " . $result['slika'] . "\n";
+        }
+    }
+    $stmt2 = $pdo->prepare("DELETE FROM fotografije WHERE id_nekretnina = $id_nekretnine ");
+    if (!$stmt2->execute()) {
+        echo $stmt2->errorCode();
+    }
+}
